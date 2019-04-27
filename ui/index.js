e9a418ac4fb33ea0d507daae8ee585de215d40d5
Oliver Wright <meiamsome@meiamso.me>
2019-04-27 14:56:20 +0100

    Add UI tip for endZones

--- ui/index.js
+++ ui/index.js
@@ -19,6 +19,26 @@ class UI {
     for (const gemDisplay of this.gemDisplays) {
       gemDisplay.draw();
     }
+    if (level) {
+      let playerHere = false;
+      let readyToLeave = false;
+      for (const entity of level.entities) {
+        if (!(entity instanceof EndZone)) continue;
+        playerHere |= entity.playerHere;
+        readyToLeave |= entity.readyToLeave;
+      }
+      if (playerHere) {
+        textSize(32);
+        stroke(0);
+        fill(255);
+        textAlign(CENTER, TOP);
+        if(readyToLeave) {
+          text("Press E to Leave", width/2, 100)
+        } else {
+          text("Defeat All Enemies To Continue", width/2, 100)
+        }
+      }
+    }
   }
 
   collectGem(gem) {
